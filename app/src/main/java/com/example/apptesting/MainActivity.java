e35e7825cca8a715497a00deea68498c954bfc35
aditi977 <aditid@vt.edu>
2021-01-30 18:26:52 -0500

    added new activity

--- app/src/main/java/com/example/apptesting/MainActivity.java
+++ app/src/main/java/com/example/apptesting/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.apptesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToAddForm(View view){
+        Intent intent = new Intent(this, AddItem.class);
+    }
+
+
+
+
+
+
+
 }
