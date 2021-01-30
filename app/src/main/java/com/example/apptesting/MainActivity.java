293c83d1aeb678b96cce9d4e652e36c584516674
ChauLe19 <chaule19@vt.edu>
2021-01-31 05:41:45 +0700

    Show date dialog

--- app/src/main/java/com/example/apptesting/MainActivity.java
+++ app/src/main/java/com/example/apptesting/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.apptesting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.DialogTitle;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void isExpired(String dateYear) {
+        try {
+            new SimpleDateFormat("MM/yyyy").parse(dateYear).compareTo(new Date());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+//    @Override
+//    protected Dialog onCreateDialog(int id) {
+//        // TODO Auto-generated method stub
+//        if (id == 999) {
+//            return new DatePickerDialog(this, myDateListener, year, month, day);
+//        }
+//        return null;
+//    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    public void showDatePickerDialog(View view) {
+
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+
     }
 }
